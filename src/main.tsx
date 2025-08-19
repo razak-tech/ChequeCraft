92ab13112954dde7f42dc9311c54402dd384e39f
razak-tech <djamel.abdou001@gmail.com>
2025-08-19 23:58:20 +0100

    Update project to fix paths and routing 2

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={(import.meta as any).env?.BASE_URL ?? '/'}>
+    <BrowserRouter basename="/ChequeCraft/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
